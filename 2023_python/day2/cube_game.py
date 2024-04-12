d2f952cb630989956e9600d85175748954c522c2
jwhunt19 <jwhunt19@gmail.com>
2024-04-11 21:09:45 -0600

    complete day 2 part 1

---
+++ 2023_python/day2/cube_game.py
@@ -0,0 +1,71 @@
+import re
+
+# raw_games = open("2023_python/day2/example.txt", "r").read() # games 1, 2 & 5 pass. sum 8
+raw_games = open("2023_python/day2/input.txt", "r").read()
+raw_games = re.split("\n", raw_games)
+
+limit = {"red": 12, "green": 13, "blue": 14} # limit set by challenge 
+
+# format data to be easily iterable
+def format_games_arr(games):
+    formatted_game = {}
+
+    for game in games:
+        sets = re.split("[;:]\s+", game)
+        game_num = sets[0][5:]
+
+        sets = sets[1:]
+        formatted_game[game_num] = []
+
+        i = 0
+        while i < len(sets):
+            formatted_game[game_num].append({})
+            set = re.split(",\s",sets[i])
+            for color in set:
+                color_data = re.split("\s", color)
+                formatted_game[game_num][i][color_data[1]] = color_data[0]
+
+            i += 1
+
+    return formatted_game
+
+# check if set in game is valid
+def valid_set(set, limit):
+    is_valid = True
+    for color in limit:
+        if set.get(color):
+            if int(set[color]) > limit[color]:
+                is_valid = False
+
+    return is_valid
+
+# check if game is possible
+def possible_game(game, limit):
+    possible = True
+    for set in game:
+        if not valid_set(set, limit):
+            possible = False
+
+
+    return possible
+
+# gets possible game ids 
+def possible_games(games, limit):
+    possible_games = []
+    for game_id in games:
+        if possible_game(games[game_id], limit):
+            possible_games.append(game_id)
+
+    return possible_games
+
+# returns the sum of all valid game ids
+def sum_games(games):
+    sum = 0
+    for val in games:
+        sum += int(val)
+    
+    return sum
+
+games = format_games_arr(raw_games) # format games
+possible_games_arr = possible_games(games, limit) # get all possible ids
+print(sum_games(possible_games_arr)) # print sum of all possible ids
