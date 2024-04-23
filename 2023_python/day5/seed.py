79434f3a331661fd389a50a4b0b0035f5707ad65
jwhunt19 <jwhunt19@gmail.com>
2024-04-23 00:11:29 -0600

    complete day 5 part 2

--- 2023_python/day5/seed.py
+++ 2023_python/day5/seed.py
@@ -1,97 +1,124 @@
 import os
-import re
-
-script_dir = os.path.dirname(os.path.realpath(__file__))
-# input_file_path = os.path.join(script_dir, "input.txt")
-input_file_path = os.path.join(script_dir, "example.txt")
-
-with open(input_file_path, encoding="utf-8") as input_file:
-    input_file = input_file.read()
 
 
-# shape raw file data
-def shape_data(file):
-    seed_list = get_seed_list(file)
-    seed_maps = {}
+# parse data from file for seeds and map data
+def parse_data(data):
+    seeds = None
+    maps = []
+    cur_map = []
 
-    maps = file.split("\n\n")[1:]
+    for i, line in enumerate(data):
+        if i == 0:
+            seeds = parse_seeds(line)
+        else:
+            # if line is nums, split to list, convert to ints
+            if line[0].isnumeric():
+                map_line = line.split()
+                map_line = list(map(int, map_line))
+                cur_map.append(map_line)
+            # else if line is blank, add current map and reset
+            elif not line.strip() and len(cur_map) > 0:
+                maps.append(cur_map)
+                cur_map = []
+
+    # add last map
+    if cur_map:
+        maps.append(cur_map)
+
+    return seeds, maps
+
+
+# parse seeds, convert to ints
+def parse_seeds(line):
+    seeds = line.split(":")[1].split()
+    seeds = list(map(int, seeds))
+
+    return seeds
+
+
+# get seed ranges for each set of seed nums
+def get_seed_ranges(seeds):
+    ranges = []
+
+    for i, _ in enumerate(seeds):
+        if i % 2 == 0:
+            seed_min = seeds[i]
+            seed_max = seeds[i] + (seeds[i + 1] - 1)
+
+            ranges.append([seed_min, seed_max])
+
+    return ranges
+
+
+# get list of locations associated with ranges
+def get_locations(ranges, maps):
     for seed_map in maps:
-        source, destination, ranges = build_map_data(seed_map)
-        seed_maps[source] = {"destination": destination, "ranges": ranges}
+        new_ranges = []
+        for seed_range in ranges:
+            segments = [seed_range]
 
-    return seed_list, seed_maps
+            # iterate over each line of a map
+            for line in seed_map:
+                l_min = line[1]  # line min
+                l_max = line[1] + (line[2] - 1)  # line max
+                diff = line[0] - line[1]
 
+                for i, segment in enumerate(segments):
+                    s_min = segment[0]  # seed min
+                    s_max = segment[1]  # seed max
 
-# converts first line of file into readable seed list
-def get_seed_list(file):
-    seed_list = file.split("\n")[0]  # grab seed list from first line of the file
-    seed_list = seed_list.split(":")  # split "seed" key from seed values
-    seed_list = seed_list[1].split()  # set to array of seeds
+                    # if entire seed range is in line range
+                    if s_min >= l_min and s_max <= l_max:
+                        segments = segments[0:i] + segments[i + 1 :]
+                        new_ranges.append([s_min + diff, s_max + diff])
 
-    return seed_list
+                    # if middle of seed range is in line range
+                    if s_min <= l_min and s_max >= l_max:
+                        segments = segments[0:i] + segments[i + 1 :]
+                        segments.append([s_min, l_min - 1])
+                        new_ranges.append([l_min + diff, l_max + diff])
+                        segments.append([l_max + 1, s_max])
 
+                    # if only end of seed range is in line range
+                    if s_min < l_min and s_max >= l_min and s_max <= l_max:
+                        segments = segments[0:i] + segments[i + 1 :]
+                        segments.append([s_min, l_min - 1])
+                        new_ranges.append([l_min + diff, s_max + diff])
 
-# shapes raw seed_map to it's revelvant parts
-def build_map_data(seed_map):
-    # splits map data into parts to be assigned (split on "-", " map", and \n)
-    seed_map_data = re.split(r"[-]|\s+(?=map)|\n", seed_map)
+                    # if only start of seed range is in line range
+                    if s_min >= l_min and s_min <= l_max and s_max > l_max:
+                        segments = segments[0:i] + segments[i + 1 :]
+                        new_ranges.append([s_min + diff, l_max + diff])
+                        segments.append([l_max + 1, s_max])
 
-    map_source = seed_map_data[0]  # set source for map
-    map_dest = seed_map_data[2]  # set destination for map
+            if segments:
+                new_ranges = new_ranges + segments
 
-    seed_ranges = seed_map_data[4:]  # assign rest of the data to ranges
-    for i, _ in enumerate(seed_ranges):  # convert each line from a string to a list
-        seed_ranges[i] = seed_ranges[i].split()
+        ranges = new_ranges
 
-    return map_source, map_dest, seed_ranges
-
-
-# gets path and location for each seed
-def get_seed_paths(seed_list, maps):
-    seed_paths_dict = {}
-
-    for seed in seed_list:
-        seed_path = []  # list of all destination numbers
-        source = "seed"  # source to reference current map
-        cur_num = int(seed)  # current source number for referencing map
-        while source:  # while there is a valid next source
-            seed_map = maps[source]
-
-
-            # TODO: add logic here to look at difference of source/dest nums
-            
-
-            seed_path.append(cur_num)  # add destination num to list
-
-            if maps.get(seed_map["destination"]):  # if the next destination exists
-                source = seed_map[
-                    "destination"
-                ]  # continue to new destination if exists
-            else:
-                source = None  # end loop if not, location has been reached
-
-        seed_paths_dict[seed] = {
-            "path": seed_path,
-            "location": seed_path[len(seed_path) - 1],
-        }
-
-    return seed_paths_dict
+    return ranges
 
 
-def get_lowest_location(paths):
-    locations = []
+def get_lowest(locations):
+    flat_locations = [item for sublist in locations for item in sublist]
 
-    for _, path in paths.items():
-        locations.append(path["location"])
+    lowest_number = min(flat_locations)
 
-    locations.sort()
-
-    return locations[0]
+    return lowest_number
 
 
-seeds, seed_maps_dict = shape_data(input_file)
+# file path
+script_dir = os.path.dirname(os.path.realpath(__file__))
+input_file_path = os.path.join(script_dir, "input.txt")
 
-# print(lowest_location)
+# read file
+with open(input_file_path, encoding="utf-8") as seed_data:
+    seed_list, map_list = parse_data(seed_data)
 
-print("seeds: ", seeds)
-print("seed_map_dict: ", seed_maps_dict)
+seed_ranges = get_seed_ranges(seed_list)
+
+seed_locations = get_locations(seed_ranges, map_list)
+
+lowest_location = get_lowest(seed_locations)
+
+print(lowest_location)
