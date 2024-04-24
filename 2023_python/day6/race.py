2681ccb0c412591a56c23a3b289f7859ceb34040
jwhunt19 <jwhunt19@gmail.com>
2024-04-23 21:15:21 -0600

    complete day 6 part 2

--- 2023_python/day6/race.py
+++ 2023_python/day6/race.py
@@ -1,8 +1,6 @@
 import os
 import functools
-import time # TODO: delete
 
-start_time = time.time()  # Get the current time in seconds
 
 # parse file data
 def parse_data(data, day):
@@ -10,11 +8,13 @@ def parse_data(data, day):
     distances = []
     for i, data in enumerate(file.readlines()):
         data = data.split(":")[1].split()
+
+        # optional flag check for day 2
         if day == 2:
+            # concat nums into one num, list for compatibility with previous code
             data = ["".join(data)]
+
         data = [int(n) for n in data]
-        
-            
 
         if i == 0:
             times = data
@@ -24,56 +24,40 @@ def parse_data(data, day):
     return times, distances
 
 
-# get times
-def get_win_counts(times, distances):
+# get win count
+def get_win_count(times, distances):
     win_num_list = []
 
     for i, _ in enumerate(times):
         t = times[i]
         d = distances[i]
-        win_nums = 0
+        outcomes = []
 
         for i in range(t):
-            cur_dist = 0
-            cur_time = 0
-            speed = 0
-            hold = True
+            # equation for getting race distance
+            outcomes.append(i * (t - i))
 
-            while cur_time < t:
-                if cur_time >= i:
-                    hold = False
-                if hold:
-                    speed += 1
-                else:
-                    cur_dist += speed
-
-                cur_time += 1
-
-            if cur_dist > d:
-                win_nums += 1
-
-        win_num_list.append(win_nums)
+        # filter out wins if greater than distance
+        wins = list(filter(lambda n: n > d, outcomes))
+        win_num_list.append(len(wins))
 
     return win_num_list
 
 
+# reduce wins to product
 def get_wins_product(wins):
     return functools.reduce(lambda a, c: a * c, wins)
 
 
 # file path
 script_dir = os.path.dirname(os.path.realpath(__file__))
-input_file_path = os.path.join(script_dir, "example.txt")
+input_file_path = os.path.join(script_dir, "input.txt")
 
 with open(input_file_path, encoding="utf-8") as file:
-    DAY = 1
+    DAY = 2
     times_list, distances_list = parse_data(file, DAY)
 
-win_list = get_win_counts(times_list, distances_list)
+win_list = get_win_count(times_list, distances_list)
 wins_product = get_wins_product(win_list)
 
 print(wins_product)
-
-end_time = time.time()  # Get the current time again
-# time taken in MM:SS:MS
-print("Time taken: ", time.strftime("%M:%S:%MS", time.gmtime(end_time - start_time)))  # TODO: delete
