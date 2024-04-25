8f5e6e3b9f34ff416b08d20564e0bb45002775ad
jwhunt19 <jwhunt19@gmail.com>
2024-04-24 19:03:39 -0600

    complete day 7 part 2

--- 2023_python/day7/cards.py
+++ 2023_python/day7/cards.py
@@ -1,28 +1,30 @@
 import os
+import copy
 
 
-def parse_hands_data(hands):
-    card_list = []
+# parse hands data into a list of hand dictionaries
+def parse_hands_data(hands_data):
+    hands = []
 
-    for hand in hands:
+    for hand in hands_data:
         # create a dictionary to store the hand
-        cards, card_score = hand.split()
-        hand = {"cards": None, "score": int(card_score)}
+        cards, card_bid = hand.split()
+        hand = {"cards": None, "bid": int(card_bid)}
 
         # get the poker hand
-        hand_score = get_poker_hand(cards)
-        hand["hand"] = hand_score
+        hand_bid = get_poker_hand(cards)
+        hand["hand"] = hand_bid
 
         # get the value of the cards
         cards = get_cards_value(cards)
         hand["cards"] = cards
 
-        card_list.append(hand)
+        hands.append(hand)
 
-    return card_list
+    return hands
 
 
-# function that returns the poker hand in current hand
+# returns poker hand value of current hand
 def get_poker_hand(hand):
     poker_hands = {
         "5": 6,  # five of a kind
@@ -34,22 +36,58 @@ def get_poker_hand(hand):
         "11111": 0,  # high card
     }
 
-    card_count = {}
-
+    # count instances of each card
+    card_counts = {}
     for card in hand:
-        if card in card_count:
-            card_count[card] += 1
+        if card in card_counts:
+            card_counts[card] += 1
         else:
-            card_count[card] = 1
+            card_counts[card] = 1
 
-    card_count = sorted(card_count.values())
-    card_count = "".join(str(x) for x in card_count)
-
-    poker_hand = poker_hands.get(card_count)
+    # get best possible hand if jokers are enabled
+    if JOKERS and card_counts.get("J"):
+        poker_hand = joker_hands(card_counts, poker_hands)
+    else:
+        poker_hand_string = get_poker_hand_string(card_counts)
+        poker_hand = poker_hands.get(poker_hand_string)
 
     return poker_hand
 
 
+# returns best possible hand with jokers
+def joker_hands(card_count, poker_hands):
+    j = card_count["J"]
+
+    possible_hands = []
+
+    for card, count in card_count.items():
+        if not card == "J":
+            hand = copy.deepcopy(card_count)
+            hand[card] = count + j
+            del hand["J"]
+            possible_hands.append(hand)
+        else:
+            possible_hands.append(card_count)
+
+    possible_hands = [get_poker_hand_string(hand) for hand in possible_hands]
+
+    best_hand = -1
+    for hand in possible_hands:
+        hand_val = poker_hands[hand]
+        if hand_val > best_hand:
+            best_hand = hand_val
+
+    return best_hand
+
+
+# returns a string of card counts
+def get_poker_hand_string(card_counts):
+    poker_hand_string = sorted(card_counts.values())
+    poker_hand_string = "".join(str(x) for x in poker_hand_string)
+    return poker_hand_string
+
+
+# returns arbitrary value of the cards
 def get_cards_value(cards):
     card_value_map = {
         "2": "12",
@@ -61,14 +99,19 @@ def get_cards_value(cards):
         "8": "18",
         "9": "19",
         "T": "20",
-        "J": "21",
         "Q": "22",
         "K": "23",
         "A": "24",
     }
 
-    cards_value = ""
+    # joker replaces jack, becomes weakest card
+    if JOKERS:
+        card_value_map["J"] = "11"
+    else:
+        card_value_map["J"] = "21"
 
+    # get value of cards based on card_value_map
+    cards_value = ""
     for card in cards:
         value = card_value_map.get(card)
         cards_value += value
@@ -76,6 +119,7 @@ def get_cards_value(cards):
     return cards_value
 
 
+# returns win count for each hand for ranking
 def get_wins(hands):
     for hand in hands:
         hand["wins"] = 0
@@ -88,9 +132,10 @@ def get_wins(hands):
     return hands
 
 
+# returns True if hand_1 wins, False otherwise
 def compare_hands(hand_1, hand_2):
-    if hand_1["hand"] == hand_2["hand"]:
-        if hand_1["cards"] > hand_2["cards"]:
+    if hand_1["hand"] == hand_2["hand"]:  # tie breaker
+        if hand_1["cards"] > hand_2["cards"]:  # compare card values
             return True
     elif hand_1["hand"] > hand_2["hand"]:
         return True
@@ -98,11 +143,12 @@ def compare_hands(hand_1, hand_2):
         return False
 
 
+# returns sum of all each hand's (bid * wins)
 def get_total_winnings(hands):
     total = 0
 
     for hand in hands:
-        total += hand["score"] * (hand["wins"] + 1)
+        total += hand["bid"] * (hand["wins"] + 1)
 
     return total
 
@@ -114,6 +160,9 @@ input_file_path = os.path.join(script_dir, "input.txt")
 # open file and split lines
 with open(input_file_path, encoding="utf-8") as file:
     data = file.read().splitlines()
+
+# joker flag (for part 2)
+JOKERS = True
 
 # parse data
 hands_list = parse_hands_data(data)
