ece3105c203c83ae078022df9fba817d9676d904
jwhunt19 <jwhunt19@gmail.com>
2024-04-29 19:47:31 -0600

    complete day 9 part 2

--- 2023_python/day9/oasis.py
+++ 2023_python/day9/oasis.py
@@ -1,27 +1,39 @@
-def next_value(history):
-    value = 0
+def get_adjacent_values(history):
+    next_value = 0
+    prev_value = 0
+
     vals = history
     next_vals = []
+    lowest_vals = []
+
     all_zeroes = False
     while not all_zeroes:
-        for i, n in enumerate(vals):
+        for i, v in enumerate(vals):
             if i != 0:
-                next_vals.append(n - vals[i - 1])
+                next_vals.append(v - vals[i - 1])
+            else:
+                lowest_vals.append(v)
 
             if i == len(vals) - 1:
-                value += n
+                next_value += v
                 all_zeroes = all([x == 0 for x in next_vals])
                 vals = next_vals
                 next_vals = []
 
-    return value
+    for i in range(len(lowest_vals) - 1, -1, -1):
+        prev_value = lowest_vals[i] - prev_value
+
+    return next_value, prev_value
 
 
-next_value_sums = 0
+NEXT_VALS_SUM = 0
+PREV_VALS_SUM = 0
 
 with open("2023_python/day9/input.txt", encoding="utf-8") as file:
     for line in file.readlines():
         history_nums = [int(n) for n in line.split()]
-        next_value_sums += next_value(history_nums)
+        next_v, prev_v = get_adjacent_values(history_nums)
+        NEXT_VALS_SUM += next_v
+        PREV_VALS_SUM += prev_v
 
-print(next_value_sums)
+print(NEXT_VALS_SUM, PREV_VALS_SUM)
